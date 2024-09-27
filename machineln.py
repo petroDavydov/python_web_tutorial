f5d8abf4a64d3de64588f2c01c83c1e32c0d66c1
Petro Davydov <78567740+petroDavydov@users.noreply.github.com>
2024-09-28 00:04:04 +0300

    Polymonial Refgession

--- machineln.py
+++ machineln.py
@@ -109,3 +109,17 @@ import matplotlib.pyplot as plt
 # plt.show()
 # -----------------------------------
 
+# Polymonial Regression
+
+x_hour = [1, 2, 3, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 18, 19, 21, 22, 23, 00]
+y_speed = [100, 90, 80, 60, 60, 55, 60, 65, 70, 70, 75, 76, 78, 79, 90, 99, 99, 100, 120, 120]
+
+
+my_model = np.poly1d(np.polyfit(x_hour, y_speed, 3))
+
+my_line = np.linspace(1, 24, 100)
+
+
+plt.scatter(x_hour, y_speed)
+plt.plot(my_line, my_model(my_line), color = 'r')
+plt.show()
